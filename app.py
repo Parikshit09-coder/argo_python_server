c57457b3413d3ee64c5fac89044fcde8e1473ea5
Parikshit09-coder <gurudesai2005@gmail.com>
2025-09-25 20:40:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,5 @@ def upload_file():
 
 # --- Run ---
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT, fallback 5000 for local
+    app.run(host="0.0.0.0", port=port)
